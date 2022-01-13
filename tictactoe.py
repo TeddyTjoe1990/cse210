72a2a5f354c8306c9b778305a446be5d55d92b9e
TeddyTjoe1990 <her21059@byui.edu>
2022-01-13 19:31:54 +0700

    Update

--- tictactoe.py
+++ tictactoe.py
@@ -1,28 +1,26 @@
-print(">Tic-Tac-Toe<")
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
+print(f"{Fore.RED}>Tic-Tac-Toe<")
+print(f"{Fore.RED} By Teddy")
+print()
 def main():
-    player = next_player("")
+    pl = upc_pl("")
     board = play_board()
     while not (win(board) or tie(board)):
         game_board(board)
-        movement(player, board)
-        player = next_player(player)
+        movement(pl, board)
+        pl = upc_pl(pl)
     game_board(board)
-    print("Good game. Thanks for playing!") 
+    print(f"{Fore.BLUE}Good game. Thanks for playing!")
+    print()
 
 def play_board():
     board = []
     for answer in range(9):
         board.append(answer + 1)
     return board
-
-def game_board(board):
-    print()
-    print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-+-+-')
-    print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-+-+-')
-    print(f"{board[6]}|{board[7]}|{board[8]}")
     
 def tie(board):
     for answer in range(9):
@@ -40,15 +38,22 @@ def win(board):
             board[0] == board[4] == board[8] or
             board[2] == board[4] == board[6])
 
-def movement(player, board):
-    answer = int(input(f"{player}'s turn to choose a square (1-9): "))
-    board[answer - 1] = player
+def movement(pl, board):
+    answer = int(input(f"{pl}'s turn to choose a square (1-9): "))
+    board[answer - 1] = pl
 
-def next_player(current):
+def upc_pl(current):
     if current == "" or current == "o":
         return "x"
     elif current == "x":
         return "o"
+
+def game_board(board):
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
 
 if __name__ == "__main__":
     main()
